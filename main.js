e1a5f151de3b91ac8b885a9e61b9d6a20fec73d6
sherif20766 <sherif20766@hotmail.com>
2017-03-16 23:50:33 +0000

    updates

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ const rand = (a, b) => Math.floor((Math.random()*b)+a);
 //prompt user for location
 const findUser = () => {
 
-	user[0] = parseInt(prompt("what is your X axis location"));
-	user[1] = parseInt(prompt("what is your Y axis location"));
+	user[0] = parseInt(prompt("Enter your X axis location (between -10 & 10)"));
+	user[1] = parseInt(prompt("Enter your Y axis location (between -10 & 10)"));
 
 	console.log("Your location is " + user);
 	console.log(closest(user[0],user[1]));
 
-	store.map(function(x){
+	store.map((x) => {
 		// displays results to console for reference
 		console.log(`Event Location ID: ${x.Event_ID} - Price: $${x.Ticket_Price}`);
 	});
@@ -35,13 +35,18 @@ const findUser = () => {
 
 //show closest events
 const closest = (x, y) => {
-store = [];
+	store = [];
+	
+	let s = 0;
+	let d = 0;
+	
+
 	//iterate in both directions
 		for(let i=0;i<3;i++){
 			for(let j=0;j<3;j++){
 				if(coord[user[0]+i][user[1]+j].Event !==0){
 		
-					if(((user[0]+i) <=10) && ((user[1]+i) <=10))    {		store.push(coord[user[0]+i][user[1]+j]);		}
+					if(((user[0]+i)<=10) && ((user[1]+i) <=10))    {		store.push(coord[user[0]+i][user[1]+j]);		}
 					
 				}
 			}
@@ -54,7 +59,6 @@ store = [];
 			}
 		}
 
-
 }
 
 //print to screen
@@ -62,25 +66,24 @@ const view = () => {
 
     let ul = document.querySelector('ul');
     ul.innerHTML = '';
-    store.sort((a,b)=>(Math.pow(a.Event_ID[0],2)+Math.pow(b.Event_ID[0],2))-(Math.pow(b.Event_ID[0],2) + Math.pow(b.Event_ID[1],2)))  //sort order of closest
-    	 
-    	 //isolate closest 5
+    store.sort((a,b)=>(Math.pow(a.Event_ID[0],2)+Math.pow(b.Event_ID[0],2))-(Math.pow(b.Event_ID[0],2) + Math.pow(b.Event_ID[1],2)))  //sort and isolate closest 5
     	 .slice(0,5).forEach(x => {
-       		let li = document.createElement('li');
-       		li.textContent = `Event Location ID: ${x.Event_ID} - Price: $${x.Ticket_Price}`
-      		ul.appendChild(li);
-    	});
+							       	let li = document.createElement('li');
+							       	li.textContent = `Event Location ID: ${x.Event_ID} - Price: $${x.Ticket_Price}`
+							      	ul.appendChild(li);
+							      });
 } 
 
 //create grid with randomly generated event ids, ticket numbers & ticket prices
-for (let i = min; i <= max;i++) {
+for (let i = min; i <= max; i++) {
  coord[i] = new Array();
  for (let j = min; j <= max;j++) {
- 	coord[i][j] = {	Event: rand(0.5,1), 			  // 50% chance of event taking place
-  					Event_ID: [i,j],				  // unque ID based on position
-  					Number_of_Tickets: rand(0, 1000), //number of tickets left
-  					Ticket_Price: rand(10, 100) 	  //ticket price between 10 & 100 pounds
- 				};
+ 	coord[i][j] = {	
+ 					Event: rand(0.5,1), 			  // 50% chance of event taking place
+  					Event_ID: [i,j],				  // unque ID based on position for ease of use
+  					Number_of_Tickets: rand(0, 1000), // number of tickets left
+  					Ticket_Price: rand(10, 100) 	  // ticket price between 10 & 100 pounds
+ 				  };
  }
 
 }
